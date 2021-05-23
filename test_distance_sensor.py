abc08409b4008caed53b4b8fa1ea60de85a53dcd
conceptroot <conceptroot@gmail.com>
2021-05-23 19:55:22 +0100

    .

--- test_distance_sensor.py
+++ test_distance_sensor.py
@@ -3,12 +3,18 @@ from gpiozero import DistanceSensor
 from statistics import median 
 import colorama
 from colorama import Fore
+from rpi_lcd import LCD
+from time import sleep
+
+
+
 
 from robot.settings import sets
 
 colorama.init(autoreset=True)
 
 distance_sensor = DistanceSensor(echo = sets['distance_echo'], trigger = sets['distance_trigger'])
+lcd = LCD(address=0x27, bus=1, width=16, rows=2)
 
 def nice_distance(distanse: float) -> float:
     SLEEP_TIME = 0.1
@@ -31,4 +37,9 @@ def nice_distance(distanse: float) -> float:
 # while True:
     # print(f"distance = {distance_sensor.distance}")
 for nice_dist in nice_distance(distance_sensor.distance):
+    lcd.text('Distance:', 1)
+    lcd.text(f'{nice_dist:.2f}', 2)
     print(Fore.RED + f"distance = {nice_dist:.2f}")
+
+
+lcd.clear()
