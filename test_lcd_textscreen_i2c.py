4f08b20122d9a5e1facb59107852a70a3d3a93d8
conceptroot <conceptroot@gmail.com>
2021-05-23 12:46:36 +0100

    add lcd text test, its working

--- test_lcd_textscreen_i2c.py
+++ test_lcd_textscreen_i2c.py
@@ -1,3 +1,13 @@
 from rpi_lcd import LCD
+from time import sleep
 
+lcd = LCD(address=0x27, bus=1, width=16, rows=2)
 
+lcd.text('Hello World!', 1)
+lcd.text('Raspberry Pi', 2)
+
+sleep(2)
+lcd.clear()
+
+lcd.text("This is test string for i2c lcd screen, which has 16x2 demension.", 1)
+sleep(5)
